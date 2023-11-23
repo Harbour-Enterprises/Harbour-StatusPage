8bd5e715c0e17764adf6a47972b6d89947afb867
Vlada Andrushchenko <dataart.vladyslava@harbourcollaborators.com>
2023-11-23 22:11:32 +0200

    subscribe to updates(UI)

---
+++ .vitepress/theme/components/Subscribe.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+import { NButton, NModal, NCard, NInput } from 'naive-ui';
+
+const showModal = ref(false);
+const email = ref('');
+</script>
+
+<template>
+  <div class="header-button">
+    <n-button @click="showModal = true">
+      Subscribe
+    </n-button>
+    <n-modal v-model:show="showModal">
+      <n-card
+        style="width: 350px"
+        title="Subscribe to updates"
+        :bordered="false"
+        size="small"
+        role="dialog"
+        aria-modal="true"
+      >
+        <n-input v-model:value="email" type="text" placeholder="Enter your email" />
+        <template #footer>
+          <n-button>
+            Subscribe via email
+          </n-button>
+        </template>
+      </n-card>
+    </n-modal>
+  </div>
+</template>
+
+<style scoped>
+.header-button {
+  margin-left: 15px;
+}
+</style>
