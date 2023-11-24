c559af32d2b7da7a0c0b89771b02a13c2bd28567
Caio Pizzol <caio@harbourshare.com>
2023-11-24 07:59:41 -0300

    refactor: hiding subscribe for now

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,12 +1,12 @@
-import { h } from 'vue';
+// import { h } from 'vue';
 import DefaultTheme from 'vitepress/theme';
-import Subscribe from './components/Subscribe.vue';
+// import Subscribe from './components/Subscribe.vue';
 
 export default {
   ...DefaultTheme,
-  Layout: h(DefaultTheme.Layout, null, {
-    'nav-bar-content-after': () => h(Subscribe),
-  }),
+  // Layout: h(DefaultTheme.Layout, null, {
+  //   'nav-bar-content-after': () => h(Subscribe),
+  // }),
   enhanceApp(ctx) {
     // extend default theme custom behaviour.
     DefaultTheme.enhanceApp(ctx);
